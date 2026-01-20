838a6f0597e063e0a9f9f74c7571812913120492
Take Profit <apaulino5@gmail.com>
2026-01-20 11:08:46 -0800

    chore: remove remaining legacy backend logic (api, auth, prisma, pump)

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
+
 import { SolanaProvider } from './SolanaProvider'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <SolanaProvider>
-            <SessionProvider>
-                {children}
-            </SessionProvider>
+            {children}
         </SolanaProvider>
     )
 }
